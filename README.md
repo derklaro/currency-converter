7442e99079e0e444c6dc453b5fd300f2ffe41265
derklaro <pasqual.koschmieder@easybill.de>
2023-03-24 09:35:13 +0100

    made output more detailed, add json route, use different currency api

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ![License](https://img.shields.io/github/license/derklaro/lira-checker)
 
-HTTP api application that just responds with the current lira status, updated every 15 minutes. A version of this web
+HTTP api application that just responds with the current lira status, updated every 30 seconds. A version of this web
 application is available at [https://lira.derklaro.dev](https://lira.derklaro.dev)
 
 ### Environment variables
 
 1. `BIND`: the address to bind the web server to in form `ip:port` or `[ipv6]:port`.
-2. `CURRENCY_API_TOKEN`: an api token for [Free Currency API](https://freecurrencyapi.com). The api will be used to fetch the lira status.
+2. `CURRENCY_API_TOKEN`: an api token for [Fast Forex](https://www.fastforex.io/). The api will be used to fetch the lira status.
 
 ### HTTP routes
 
@@ -16,14 +16,52 @@ application is available at [https://lira.derklaro.dev](https://lira.derklaro.de
 
 [Try it out](https://lira.derklaro.dev/status)
 
-Responds with the current lira status, in a nice one-line text format. The response looks like: `Lira status: 1 Lira is equal to 0.048295 Euro (0.052496 US-Dollar)`.
+Responds with the current lira status, in a nice one-line text format. The response looks like: `Lira Status as of 2023-03-24 08:14:40 (UTC): 1 Lira is equal to 0.04859 Euro (0.05245 US-Dollar)`.
 
 Possible response codes are:
 
-| Status | Meaning                                                                                        |
-|--------|------------------------------------------------------------------------------------------------|
-| 200    | Ok, but no status has to be available. In that case the response body is `No status available` |
-| 500    | An error occurred while processing the request.                                                |
+| Status | Meaning                                         |
+|--------|-------------------------------------------------|
+| 200    | Data is available and formatted.                |
+| 204    | No data is yet available.                       |
+| 500    | An error occurred while processing the request. |
+
+#### `/status/json`
+
+[Try it out](https://lira.derklaro.dev/status/json)
+
+Responds with the current lira status in a json encoded format. The response looks like:
+```json
+{
+    "base": "TRY",
+    "results": {
+        "MZN": 3.34824,
+        "BHD": 0.01976,
+        "CLF": 0.00117,
+        "DZD": 7.13641,
+        "ZAR": 0.95221,
+        "XCD": 0.14154,
+        "GBP": 0.0428,
+        "THB": 1.78672,
+        "SEK": 0.54328,
+        "QAR": 0.19132,
+        "MMK": 110.11418,
+        "XDR": 0.03908,
+        "PLN": 0.22755,
+        "TJS": 0.5707,
+        ...
+    },
+    "updated": "2023-03-24 08:15:41"
+}
+```
+
+Possible response codes are:
+
+| Status | Meaning                                         |
+|--------|-------------------------------------------------|
+| 200    | Data is available and formatted.                |
+| 204    | No data is yet available.                       |
+| 500    | An error occurred while processing the request. |
 
 ### Compile from source
 
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use axum::body::Body;
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::{routing, Extension, Router, Server};
+use axum::response::{IntoResponse, Response};
+use axum::{routing, Extension, Json, Router, Server};
 use scheduled_thread_pool::ScheduledThreadPool;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::env;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 struct CurrencyApiResponse {
-    data: LiraStatus,
+    base: String,
+    #[serde(rename = "results")]
+    result: MultiFetchResult,
+    updated: String,
 }
 
-#[derive(Deserialize, Debug)]
-struct LiraStatus {
-    #[serde(rename = "EUR")]
-    in_eur: f64,
-    #[serde(rename = "USD")]
-    in_usd: f64,
+#[derive(Deserialize, Serialize, Clone, Debug)]
+struct MultiFetchResult {
+    #[serde(flatten)]
+    rates: HashMap<String, f64>,
 }
 
 #[derive(Clone, Debug)]
-struct LiraInfoHolder(Arc<Mutex<Option<String>>>);
+struct LiraInfoHolder(Arc<Mutex<Option<CurrencyApiResponse>>>);
 
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
@@ -36,28 +38,33 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
     // schedule lira status updates
     let updating_holder = lira_info_holder.clone();
     let thread_pool = ScheduledThreadPool::new(1);
-    thread_pool.execute_at_fixed_rate(
-        Duration::from_secs(1),
-        Duration::from_secs(15 * 60),
-        move || {
-            println!("Requesting latest lira information");
-            let request_url = format!("https://api.freecurrencyapi.com/v1/latest?apikey={}&currencies=USD%2CEUR&base_currency=TRY", currency_api_token);
+    thread_pool.execute_at_fixed_rate(Duration::from_secs(1), Duration::from_secs(30), move || {
+        println!("Requesting latest lira information");
+        let request_url = format!(
+            "https://api.fastforex.io/fetch-all?from={}&api_key={}",
+            "TRY", currency_api_token
+        );
 
-            let opt_response = reqwest::blocking::get(request_url);
-            if let Ok(response) = opt_response {
+        let opt_response = reqwest::blocking::get(request_url);
+        match opt_response {
+            Ok(response) => {
                 let opt_parsed_body = response.json::<CurrencyApiResponse>();
                 if let Ok(response) = opt_parsed_body {
-                    let status = format!("Lira status: 1 Lira is equal to {} Euro ({} US-Dollar)", response.data.in_eur, response.data.in_usd);
                     if let Ok(mut guard) = updating_holder.0.lock() {
-                        *guard = Some(status);
+                        *guard = Some(response);
                     }
                 }
             }
-        },
-    );
+            Err(err) => println!("Unable to fetch lira data: {}", err),
+        }
+    });
 
     let router: Router<(), Body> = Router::new()
-        .route("/status", routing::get(handle_lira_status_request))
+        .route("/status", routing::get(handle_lira_status_request_plain))
+        .route(
+            "/status/json",
+            routing::get(handle_lira_status_request_json),
+        )
         .layer(Extension(lira_info_holder));
 
     let address = bind_host
@@ -70,17 +77,41 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
     Ok(())
 }
 
-async fn handle_lira_status_request(
+async fn handle_lira_status_request_plain(
     Extension(holder): Extension<LiraInfoHolder>,
 ) -> impl IntoResponse {
     match holder.0.lock() {
-        Ok(guard) => (
-            StatusCode::OK,
-            guard.clone().unwrap_or(String::from("No status available")),
-        ),
+        Ok(guard) => guard
+            .as_ref()
+            .map(|data| {
+                let in_eur = data.result.rates.get("EUR").unwrap_or(&-1.0);
+                let in_usd = data.result.rates.get("USD").unwrap_or(&-1.0);
+                let formatted = format!(
+                    "Lira Status as of {} (UTC): 1 Lira is equal to {} Euro ({} US-Dollar)",
+                    data.updated, in_eur, in_usd
+                );
+                (StatusCode::OK, formatted)
+            })
+            .unwrap_or((StatusCode::NO_CONTENT, String::from("No status available"))),
         Err(_) => (
             StatusCode::INTERNAL_SERVER_ERROR,
-            String::from("Internal error"),
+            String::from("Unable to get latest data"),
         ),
+    }
+}
+
+async fn handle_lira_status_request_json(Extension(holder): Extension<LiraInfoHolder>) -> Response {
+    match holder.0.lock() {
+        Ok(guard) => guard
+            .as_ref()
+            .map(|data| (StatusCode::OK, Json(data.clone())).into_response())
+            .unwrap_or(
+                (StatusCode::NO_CONTENT, String::from("No status available")).into_response(),
+            ),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            String::from("Unable to get latest data"),
+        )
+            .into_response(),
     }
 }
